24775bcecd69933c4617071b3d5edfeaf7e995fe
Olin Kirkland <me@olinkirk.land>
2022-03-13 00:22:53 +0100

    Forgot to add the new Social file

---
+++ src/components/Social.jsx
@@ -0,0 +1,36 @@
+function Social() {
+  return (
+    <ul className="social-links">
+      <li>
+        <a href="https://github.com/olinkirkland" target="_blank">
+          <i className="fab fa-github"></i>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/olin-kirkland-10483078/"
+          target="_blank"
+        >
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/olinkirkland/" target="_blank">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/itsOlinKirkland" target="_blank">
+          <i className="fab fa-twitter"></i>
+        </a>
+      </li>
+      <li>
+        <a href="mailto:me@olinkirk.land" target="_blank">
+          <i className="fas fa-envelope"></i>
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Social;
