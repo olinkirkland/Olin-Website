4d8070b2729bd6c737a33789b93014061e74bd47
Olin Kirkland <me@olinkirk.land>
2022-11-23 18:24:59 +0100

    Mixpanel troubleshooting

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,34 @@ import Skills from './components/Skills';
 import { useEffect } from 'react';
 import mixpanel from 'mixpanel-browser';
 
+const MIXPANEL_TOKEN = '804056956bf1195939b17e5f559fad89';
+
 function App() {
   useEffect(() => {
-    // Track page view (mixpanel)
-    mixpanel.init('804056956bf1195939b17e5f559fad89', {
-      api_host: 'https://api-eu.mixpanel.com/track',
-      debug: true
+    mixpanel.init(MIXPANEL_TOKEN, {
+      debug: true,
+      loaded: (m) => {
+        console.log('Mixpanel loaded');
+        console.log(m);
+        m.track('test');
+      },
+      error: (err) => {
+        console.log('Mixpanel error', err);
+      }
     });
-    setTimeout(() => {
-      mixpanel.track('Page View', {
-        url: window.location.href,
-        referrer: document.referrer
-      });
-    }, 1000);
+
+    // Track page view (mixpanel)
+    // mixpanel.init('804056956bf1195939b17e5f559fad89', {
+    //   api_host: 'https://api-eu.mixpanel.com/track',
+    //   debug: true,
+    //   loaded: function (mixpanel) {
+    //     console.log('Mixpanel loaded');
+    //     mixpanel.track('Page View', {
+    //       url: window.location.href,
+    //       referrer: document.referrer
+    //     });
+    //   }
+    // });
   }, []);
 
   return (
